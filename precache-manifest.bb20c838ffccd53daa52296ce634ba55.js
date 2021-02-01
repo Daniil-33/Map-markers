2a08b2ae6651e809a6c3ee4b5ded75b11cdcfa4d
Daniil33 <daniilkostyuk33@gmail.com>
2021-02-01 02:00:35 +0200

    dabug timelaps

---
+++ precache-manifest.bb20c838ffccd53daa52296ce634ba55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00d8c49904d8dc1fd0ab",
+    "url": "/Map-markers/css/app.e484201a.css"
+  },
+  {
+    "revision": "37184fbf58efa18d7a78fdfc09d29274",
+    "url": "/Map-markers/index.html"
+  },
+  {
+    "revision": "00d8c49904d8dc1fd0ab",
+    "url": "/Map-markers/js/app.dc3114ae.js"
+  },
+  {
+    "revision": "2b50986be49402e1c874",
+    "url": "/Map-markers/js/chunk-vendors.1951795a.js"
+  },
+  {
+    "revision": "01e20585d462f41f9965a4f50f350446",
+    "url": "/Map-markers/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Map-markers/robots.txt"
+  }
+]);
